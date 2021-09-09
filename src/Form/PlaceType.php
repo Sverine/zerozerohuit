b53630f4264be1667a7d29db18e2e948f940cb84
Sverine <severine.sylvan@gmail.com>
2021-09-09 16:46:15 +0200

    add target entity and all routes of it

--- src/Form/PlaceType.php
+++ src/Form/PlaceType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Place;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,19 @@ class PlaceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('code')
-            ->add('address')
-            ->add('country')
-            ->add('type')
+            ->add('code', TextType::class, ['label'=>'Code'])
+            ->add('address', TextareaType::class,['label'=>'Adresse'])
+            ->add('country', CountryType::class, ['label'=>'Pays'])
+            ->add('type', ChoiceType::class,[
+                'label'=>'Type',
+                'choices'=>[
+                    'Grand luxe - Palace'=>'Palace',
+                    'Luxe - Hotel 5*'=>'Hotel 5*',
+                    'Confortable - Hotel Moyen'=>'Hotel confortable',
+                    'Economique - Formule 1'=>'Hotel économique',
+                    'Gratuit - Tente'=>'Tente'
+                ]
+            ])
         ;
     }
 
